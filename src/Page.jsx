32e7a9f69cdc84e562bcd402701343168463ce5e
ShafiulTuhin <tuhin6488@gmail.com>
2024-10-31 16:02:11 +0600

    Send to repository

---
+++ src/Page.jsx
@@ -0,0 +1,24 @@
+import Navigation from "./Navigation";
+import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
+import Footer from "./Footer";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
+
+const Page = () => {
+  const { darkMode } = useContext(ThemeContext);
+  return (
+    <div className={`h-full w-full ${darkMode ? "dark" : ""}`}>
+      <Navigation />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
